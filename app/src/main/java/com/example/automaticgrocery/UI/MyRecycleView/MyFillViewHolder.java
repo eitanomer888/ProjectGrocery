d8943582c69267367eb0d1cbbe95e2202527b5db
Owner <eitanomer888@gmail.com>
2024-03-13 17:24:43 +0200

    simple onclick recycleView addition

--- app/src/main/java/com/example/automaticgrocery/UI/MyRecycleView/MyFillViewHolder.java
+++ app/src/main/java/com/example/automaticgrocery/UI/MyRecycleView/MyFillViewHolder.java
@@ -3,6 +3,7 @@ package com.example.automaticgrocery.UI.MyRecycleView;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -19,5 +20,13 @@ public class MyFillViewHolder extends RecyclerView.ViewHolder {
         tvFillAmount = itemView.findViewById(R.id.tvFillAmount);
         tvFillint = itemView.findViewById(R.id.tvFillint);
         ivFillWarn = itemView.findViewById(R.id.ivFillWarn);
+
+        //on click method
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(itemView.getContext(), tvFillName.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
